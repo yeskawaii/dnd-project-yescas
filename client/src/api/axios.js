bafdc04f780f3d1c0d507119c296a1d0ca40df4d
Julio Alejandro Yescas Tinoco <yescas@MacBook-Air-de-Julio.local>
2026-04-07 10:19:03 -0600

    bugfix: corrigiendo el axios para eliminar cache con actualizaciones

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:4001/api"
 });
 
-// Este "Interceptor" pega el token en cada petición que hagamos
+// 1. El que manda el token (El que ya tenías)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -12,5 +12,20 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// 2. EL CADENERO (Lo nuevo que auto-limpia la sesión)
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token');
+
+      window.location.href = '/'; 
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
